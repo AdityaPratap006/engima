e21902b88801def162b6e5abf5649f208fc282d9
Aditya Pratap <adityapratap7344@gmail.com>
2020-01-17 21:06:22 +0530

    added navbar and  created login page component

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './navbar.style.scss'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='app-title'>
+                <h5>Admin Panel</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
